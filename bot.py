bd3639c34e8e82fc3032e0733a8ac809f70d6679
nkosinathintuli <nkosinathintuli21@gmail.com>
2021-07-02 07:40:35 +0200

    added bot funtionality: greet new member, respond to 99!

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -9,6 +10,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+"""
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
@@ -16,5 +18,48 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+"""
 
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+# Welcoming New Members
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+# Responding to Messages
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+        
 client.run(TOKEN)
